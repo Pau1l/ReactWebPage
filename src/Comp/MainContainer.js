decf23d60eb2b210b7681e821cad18b7ae558bd9
BuildTools <unconfigured@null.spigotmc.org>
2022-08-11 17:35:34 +0200

    Fixed more vars.

--- src/Comp/MainContainer.js
+++ src/Comp/MainContainer.js
@@ -4,7 +4,6 @@ import { makeStyles, AppBar} from '@material-ui/core';
 
 const useStyles = makeStyles({
     MainBar:{
-        position: 'static',
         background: 'linear-gradient(45deg, rgba(27, 27, 27, 0.2) 30%, rgba(27,27,27, 0.5) 90%)',
         display: 'flex',
         justifyContent: 'center',
@@ -12,8 +11,9 @@ const useStyles = makeStyles({
         textAlign: 'center',
         fontFamily: 'Nunito',
         width: '100%',
-        height: '500px',
+        height: '100%',
         bottom: 0,
+        overflow: 'hidden',
     },
     AppbarWrapper:{
         margin: '0 auto',
